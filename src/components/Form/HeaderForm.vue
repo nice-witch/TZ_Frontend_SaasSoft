531173723f8b9dc3579686741921aecda5b52d18
nice-witch <78809819+nice-witch@users.noreply.github.com>
2026-02-05 11:54:45 +0500

    feat: add header form

---
+++ src/components/Form/HeaderForm.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {defineEmits} from "vue";
+import Button from 'primevue/button';
+import AddIcon from "@/assets/icons/AddIcon.vue"
+
+const emit = defineEmits<{
+  (e: 'addForm'): void
+}>()
+</script>
+
+<template>
+  <div class="header-form">
+    <h1>Учетные записи</h1>
+    <Button severity="secondary" @click="emit('addForm')">
+      <AddIcon />
+    </Button>
+  </div>
+</template>
+
+<style scoped>
+.header-form {
+  margin-bottom: 40px;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+</style>
