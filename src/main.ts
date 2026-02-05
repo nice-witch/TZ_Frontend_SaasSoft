ccf9262bf53c7f37e6686605a2299d34b01a28d9
nice-witch <78809819+nice-witch@users.noreply.github.com>
2026-02-05 11:22:04 +0500

    feat: add PrimeVue library

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue);
 
 app.mount('#app')
