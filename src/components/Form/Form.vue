2c02b26964e72e2e0e711acf1ef8f6855359dd7d
nice-witch <78809819+nice-witch@users.noreply.github.com>
2026-02-05 11:57:12 +0500

    feat: add info component

---
+++ src/components/Form/Form.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import HeaderForm from './HeaderForm.vue'
+import InformationForm from './InformationForm.vue'
+
+const isClick = ref(false)
+</script>
+
+<template>
+  <HeaderForm @add-form="isClick = true" />
+  <InformationForm />
+</template>
+
+<style scoped></style>
---
+++ src/components/Form/InformationForm.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import InfoIcon from '@/assets/icons/InfoIcon.vue'
+</script>
+
+<template>
+  <div class="information-form">
+    <InfoIcon />
+    <p>Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</p>
+  </div>
+</template>
+
+<style scoped>
+.information-form {
+  margin-bottom: 20px;
+  padding: 8px 10px;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  background-color: rgba(218, 218, 218, 0.63);
+  border-radius: 8px;
+}
+</style>
