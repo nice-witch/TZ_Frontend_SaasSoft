e0b5a857bf3ecc31c5cca6823dc4aaca40da9d5c
nice-witch <78809819+nice-witch@users.noreply.github.com>
2026-02-05 11:53:51 +0500

    fix: delete basic files

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(PrimeVue);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
 app.mount('#app')
